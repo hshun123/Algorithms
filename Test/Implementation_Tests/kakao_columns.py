41f6e82de20760285aaffb498c4fb88f36d0b6e5
hshun123 <hshun123@gmail.com>
2021-07-03 18:17:14 +0900

    adding kakao_columns.py

---
+++ Test/Implementation_Tests/kakao_columns.py
@@ -0,0 +1,36 @@
+# https://programmers.co.kr/learn/courses/30/lessons/60061
+
+# check if the structure is available
+def possible(answer):
+    for x, y, stuff in answer:
+        if stuff == 0: # column
+            if y == 0 or [x-1, y, 1] in answer or [x, y, 1] in answer or [x,y - 1, 0] in answer:
+                continue
+            return False
+
+
+        elif stuff == 1:
+            if [x,y - 1, 0] in answer or [x + 1, y - 1, 0] in answer or ([x-1, y, 1] in answer and [x + 1, y, 1] in answer):
+                continue
+
+            return False
+        
+
+    return True
+
+def solution(n, build_frame):
+    answer = []
+    for frame in build_frame:
+        x, y, stuff, operate = frame
+        if operate == 0:
+            answer.remove([x,y,stuff])
+            if not possible(answer):
+                answer.append([x,y,stuff])
+
+        if operate == 1:
+            answer.append([x,y,stuff])
+            if not possible(answer):
+                answer.remove([x,y,stuff])
+
+    return sorted(answer)
+
