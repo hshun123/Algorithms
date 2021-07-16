ba5eff4741fd49027aa923b11b7da7108d7c4e7b
hshun123 <hshun123@gmail.com>
2021-07-16 11:49:31 +0900

    adding a new file kakao.py

---
+++ Test/Binary_search.py/kakao.py
@@ -0,0 +1,25 @@
+# https://programmers.co.kr/learn/courses/30/lessons/60060
+
+from bisect import bisect_left, bisect_right
+
+def count_by_range(a, left_value, right_value):
+    right_index = bisect_right(a, right_value)
+    left_index = bisect_left(a, left_value)
+    
+    return right_index - left_index
+
+array = [[] for _ in range(10001)]
+reversed_array = [[] for _ in range(10001)]
+
+def solution(words, queries):
+    answer = []
+
+    for word in words:
+        array[len(word)].append(word)
+        reversed_array[len(word)].append(word[::-1])
+
+    for i in range(10001):
+        array[i].sort()
+        reversed_array.sort()
+
+    for q
